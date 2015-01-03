1f3ee14bd88a8e929e034e4b2450dbba0af5d294
Matthew Cullum <creativelyconcise@gmail.com>
2015-01-03 02:08:33 -0800

    Fixed manifest issue

--- app/scripts/background/main.js
+++ app/scripts/background/main.js
@@ -1,5 +1,5 @@
 var scraper = require('./scraper.js');
-
+console.log(scraper)
 //var permuter = require('./permuter.js');
 //var find_last_names = require('./last_names.js');
 //var email_verifier = require('./email_check.js');
