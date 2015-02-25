72f0105d54d7f6d7695f40757a685b9d83db6434
Matthew Cullum <creativelyconcise@gmail.com>
2015-02-25 09:54:20 -0700

    Fixed results won't download

--- app/scripts/app/results.js
+++ app/scripts/app/results.js
@@ -3,29 +3,35 @@
  */
 app.results = function () {
     function invokeCSVDownload() {
-        var people = app.bp.app.results;
+        var companies = app.bp.app.results;
         var csv = "FirstName,LastName,Title,Company,Email,Email Confirmed,Profile URL\n";
 
-        $.each(people, function (index, person) {
+        $.each(companies, function (index, company) {
+            $.each(company, function (index, person) {
+                var dataString = [
+                    person.name.first || '',
+                    person.name.last || '',
+                    person.currentPosition || "",
+                    person.companyName,
+                    person.email || '',
+                    person.emailConfirmed,
+                    person.profileLink
+                ].map(function (item) {
+                        return '"' + item + '"'
+                    });
 
-            var dataString = [
-                person.name.first || '',
-                person.name.last || '',
-                person.headline.replace(/ at(.*)/, "").trim(),
-                person.companyName,
-                person.email || '',
-                person.emailConfirmed,
-                person.profileLink
-            ].map(function (item) {
-                    return '"' + item + '"'
-                });
-
-            dataString = dataString.join(',');
-            csv += index < people.length ? dataString + "\n" : dataString;
+                dataString = dataString.join(',');
+                //csv += index < companies[(companies.length-1)].length ? dataString + "\n" : dataString;
+                csv += dataString + "\n"
+            });
+        });
+        var name = '';
+        $.each(companies, function (index, item) {
+            name += item[0].companyName
         });
         var pom = document.createElement('a');
         pom.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv));
-        pom.setAttribute('download', app.bp.app.results[0].companyName.trim() + 'Employees.csv');
+        pom.setAttribute('download', name + 'Employees.csv');
         pom.click();
     }
 
